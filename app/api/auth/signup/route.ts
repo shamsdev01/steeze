3afd6a10c242f8b062d43057b539a9c21801eede
ajasa Shamsudeen <shamsudeenajasa@gmail.com>
2024-10-05 22:42:27 +0100

    done

--- app/api/auth/signup/route.ts
+++ app/api/auth/signup/route.ts
@@ -42,6 +42,7 @@ export async function POST(request: Request) {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const { password: _ , ...userWithoutPassword } = user;
     return NextResponse.json(userWithoutPassword);
+
     
   } catch (error) {
     console.error('Signup error:', error);
