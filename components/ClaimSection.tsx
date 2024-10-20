fe22557a81f192583dfa2849a1fd9b4e46c6eb8d
ajasa Shamsudeen <shamsudeenajasa@gmail.com>
2024-10-20 03:15:31 +0100

    final update

--- components/ClaimSection.tsx
+++ components/ClaimSection.tsx
@@ -1,6 +1,6 @@
 'use client'
 import React, { useState } from 'react';
-import { ChevronRight } from 'lucide-react';
+// import { ChevronRight } from 'lucide-react';
 
 const ClaimSection: React.FC = () => {
   const [selectedOption, setSelectedOption] = useState('');
